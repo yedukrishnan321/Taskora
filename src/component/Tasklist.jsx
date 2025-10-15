6687ee7a44633b0f211e8f9cc3dd652c4a6550c1
yedu krishnan <yedukrishnan321@gmail.com>
2025-10-15 10:57:34 +0530

    second commit

--- src/component/Tasklist.jsx
+++ src/component/Tasklist.jsx
@@ -1,36 +1,3 @@
-
-// export default function Tasklist({tasks}, {updateTask}, {deleteTask}) {
-    
-//     const toggleComplete = (index) => {
-//         const updatedTask = { ...tasks[index], completed: !tasks[index].completed };
-//         updateTask(updatedTask, index);
-//     };
-
-//     return(
-//         <div>
-//             <ul>
-//                 {tasks.map((task,index)=> {
-//                     <li key ={index}>
-//                         <div>
-//                             <span>{task.text}</span>
-//                             <small>({task.priority} {task.category})</small>
-//                         </div>
-
-//                         <div>
-//                             <button onClick={() => toggleComplete(index)}>
-//                             {task.completed ? "undo" : "complete"}
-//                             </button>
-                            
-//                             <button onClick={() => deleteTask(index)}>Delete</button>
-//                         </div>
-//                     </li>
-//                 })}
-//             </ul>
-//         </div>
-//     )
-// }
-
-
 export default function Tasklist({ tasks, updateTask, deleteTask }) {
 
   const toggleComplete = (index) => {
